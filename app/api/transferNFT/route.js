8559ffa8e300dcbfd1ec538e8b85c1d8f6b8f1d9
rishabBit <rishab@beimagine.tech>
2024-04-17 14:10:43 +0530

    transfer certificate change

--- app/api/transferNFT/route.js
+++ app/api/transferNFT/route.js
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server'
 const nearAPI = require('near-api-js')
-const BN = require('bn.js');
+const BN = require('bn.js')
 
 const { connect } = nearAPI
 
 export async function POST(request) {
+  console.log('transfer nft')
   const data = await request.json()
 
   const tokenId = data.tokenId
@@ -14,12 +15,11 @@ export async function POST(request) {
   const networkType = data.networkType
   const privateKey = data.privateKey.slice(8)
 
-
-  // const tokenId = "8" 
-  // const ownerId = "b4090c6b024c377897bc04c11fe29961d58edc4d287be8fcd45e7b14738e72b5" 
+  // const tokenId = "8"
+  // const ownerId = "b4090c6b024c377897bc04c11fe29961d58edc4d287be8fcd45e7b14738e72b5"
   // const contractId = "vickyx.testnet"
   // const recipient = "a658422f81304f3ce227150a754cebd714c8a2c079a0b5f59b65e31b092ea938"
-  // const networkType = "testnet" 
+  // const networkType = "testnet"
   // const privateKey = "ed25519:ddmVFbMFF5xSvbbbgex83ZvtK1qqZhP9rBrPPLCyw7Xt71gCNyWH5K923pe9TotcFqM2pXswNmjjtDQCQQscMtY"
 
   const keyPair = nearAPI.utils.KeyPair.fromString(privateKey)
@@ -53,11 +53,9 @@ export async function POST(request) {
       30_000_000_000_000, // attached GAS (optional)
       new BN('1')
     )
-    // return {status: true, data: res};
-    console.log(res)
     return NextResponse.json({ txRespone: JSON.stringify(res) })
   } catch (error) {
-    console.log(`Error occured while transferring NFT:${error}`)
-    return NextResponse.json({ txRespone: JSON.stringify(error) })
+    console.log(error)
+    console.log(`Error occured while transferring NFT`)
   }
 }
